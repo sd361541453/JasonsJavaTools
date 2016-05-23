0e9022891cb01cd09994fe132809655de474b305
ZJC <jason8home@gmail.com>
2016-05-23 16:04:34 +0800

    Change-Id: Ic065fab10670bd042a4f36b076340b8ca147deaf Signed-off-by: ZJC <jason8home@gmail.com>

---
+++ Z0523-JasonTools/src/com/jason/util/Test.java
@@ -0,0 +1,26 @@
+package com.jason.util;
+
+import java.io.File;
+
+public class Test {
+
+	public Test() {
+		long tt = JasonFileTools.fileCrc(new File("d://test/default/default.txt"), 1024);
+		System.out.println(tt);
+		tt = JasonFileTools.fileCrc(new File("d://test/default/default.txt"), 102400);
+		System.out.println(tt);
+		tt = JasonFileTools.fileCrc(new File("d://test/default/default.txt"), 10240000);
+		System.out.println(tt);
+		tt = JasonFileTools.fileCrc(new File("d://test/default/default.txt"), 102400000);
+		System.out.println(tt);
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Test();
+	}
+
+}
